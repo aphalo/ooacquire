a3085a6ccb48066de9009d77e7442f49d3b964db
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2023-04-25 10:08:22 +0300

    Add transmittance data for PC filter
    Add these data to the data-raw folder of MAYP12278 for the record. They are not included in the package as distributed.

---
+++ data-raw/calibrations-MAYP11278/PC-disk-and-dome-Tfr.R
@@ -0,0 +1,70 @@
+library(photobiology)
+library(photobiologyInOut)
+library(ggspectra)
+library(lubridate)
+library(photobiologyFilters)
+
+theme_set(theme_bw())
+
+# disk
+
+pc_disk.tb <-
+  read.csv(file = "data-raw/calibrations-MAYP11278/MAYPC.CSV",
+           sep = ";",
+           skip = 1,
+           header = FALSE,
+           col.names = c("w.length", "Tpc", "SD"))
+
+pc_disk.spct <- as.filter_spct(pc_disk.tb,
+                               Tfr.type = "total",
+                               thickness = 2e-3,
+                               attenuation.mode = "absorption",
+                               Rfr.constant = NA_real_)
+
+when_measured(pc_disk.spct) <- ymd("2023-04-24")
+what_measured(pc_disk.spct) <-
+  "Polycarbonate disk used to measure stray light with MAYP11278. Tfr measured after several years of use."
+comment(pc_disk.spct) <-
+  "Disk material polycarbonate sheet from Arla plast (Sweden), bought from Foiltek (Finland)."
+how_measured(pc_disk.spct) <-
+  "Measured with an Agilent 8453 array spectrophotometer using deuterium + tungsten lamp. No integrating sphere."
+
+autoplot(pc_disk.spct)
+
+## dome
+
+pc_dome.tb <-
+  read.csv(file = "data-raw/calibrations-MAYP11278/DOME2.CSV",
+           sep = ";",
+           skip = 1,
+           header = FALSE,
+           col.names = c("w.length", "Tpc", "SD"))
+
+pc_dome.spct <- as.filter_spct(pc_dome.tb,
+                               Tfr.type = "total",
+                               thickness = 1e-3,
+                               attenuation.mode = "absorption",
+                               Rfr.constant = NA_real_)
+pc_dome.spct <- despike(pc_dome.spct, z.threshold = 4)
+
+when_measured(pc_dome.spct) <- ymd("2023-04-24")
+what_measured(pc_dome.spct) <-
+  "Polycarbonate dome used with MAYP11278 (new)."
+comment(pc_dome.spct) <-
+  "Polycarbonate dome used to measure stray light with MAYP11278. Tfr measured when new. Dome as used in surveilance cameras described as \"51.4x26.57mm 2 Inch Small Acrylic PC Clear Dome Glass Protective Cover Security CCTV Camera Housing Anti-Aging Transparent Shell\", bought from AliExpress
+seller YiChangWang".
+how_measured(pc_dome.spct) <-
+  "Measured with an Agilent 8453 array spectrophotometer using deuterium + tungsten lamp. No integrating sphere."
+
+autoplot(pc_dome.spct,
+         annotations = c("+", "title:what:when:how"))
+
+PC_filters.mspt <- normalize(filter_mspct(list(dome = pc_dome.spct, disk = pc_disk.spct)), norm = 1050)
+
+autoplot(PC_filters.mspt)
+
+autoplot(c(PC_filters.mspt,
+           normalize(filters.mspct["Foiltek_Clear_PC"], norm = 1050)))
+
+autoplot(c(PC_filters.mspt["dome"],
+           normalize(filters.mspct["Foiltek_Clear_PC"], norm = 1050)))
