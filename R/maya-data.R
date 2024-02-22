3478c0c524be868cbfb2b4a3eed5a237734fca2a
Pedro Aphalo <pedro.aphalo@helsinki.fi>
2024-02-22 23:35:25 +0200

    Consolidate data docs
    Document all data objects using one page per spectrometer

--- R/maya-data.R
+++ R/maya-data.R
@@ -1,261 +1,115 @@
-#' Instrument descriptors for a Maya2000 Pro  spectrometer
+#' Maya2000 Pro  spectrometer s/n MAYP11278
 #'
 #' List of instrument descriptors, each one containing the calibration
 #' data needed to convert raw data acquired on different dates. Calibration
-#' data from STUK's non-ionising radiation laboratory.
+#' data from STUK's non-ionising radiation laboratory. Correction method
+#' algorithms for stray light and slit function by Lasse Ylianttila, and
+#' variations by Pedro J. Aphalo..
 #'
 #' @docType data
 #' @keywords datasets
-#' @format A list.
+#' @format Lists and data frames.
 #'
 #' @family objects containing instrument-specific data
 #'
 "MAYP11278_descriptors"
 
-#' Dates for which specific calibrations are valid.
-#'
-#' Data frame with data used to match raw spectral data to different
-#' calibrations based on the date data was acquired.
-#'
-#' @docType data
-#' @keywords datasets
-#' @format A list.
-#'
-#' @family objects containing instrument-specific data
+#' @rdname MAYP11278_descriptors
 #'
 "MAYP11278_calib_dates.df"
 
-#' Correction method definition
-#'
-#' A list with data needed to process raw data in the example files. It
-#' contains constants and functions defining the method developed by Lasse
-#' Ylianttila.
-#'
-#' @docType data
-#' @keywords datasets
-#' @format A list.
-#'
-#' @family objects containing instrument-specific data
+#' @rdname MAYP11278_descriptors
 #'
 "MAYP11278_ylianttila.mthd"
 
-#' Correction method definition
-#'
-#' A list with data needed to process raw data in the example files. It
-#' contains constants and functions defining the method developed by Lasse
-#' Ylianttila. Method modified to use 10 nm shorter wavelengths as the
-#' reference region for matching filter and dark spectra. EXPERIMENTAL!!
-#'
-#' @docType data
-#' @keywords datasets
-#' @format A list.
-#'
-#' @family objects containing instrument-specific data
+#' @rdname MAYP11278_descriptors
 #'
 "MAYP11278_short_flt_ref.mthd"
 
-#' Correction method definition
-#'
-#' List with data needed to process raw data in the example files. It
-#' contains constants and functions defining a derivative of the method
-#' developed by Lasse Ylianttila.
-#'
-#' @docType data
-#' @keywords datasets
-#' @format A list.
-#'
-#' @family objects containing instrument-specific data
+#' @rdname MAYP11278_descriptors
 #'
 "MAYP11278_sun.mthd"
 
-#' Correction method definition
-#'
-#' List with data needed to process raw data in the example files. It
-#' contains constants and functions defining a simplified method that includes
-#' some corrections developed by Lasse Ylianttila, but is less effective in
-#' controlling stray light. Should be used only when more advanced methods
-#' cannot be applied.
-#'
-#' @docType data
-#' @keywords datasets
-#' @format A list.
-#'
-#' @family objects containing instrument-specific data
+#' @rdname MAYP11278_descriptors
 #'
 "MAYP11278_simple.mthd"
 
 ################
 
-#' Instrument descriptors for a Maya2000 Pro  spectrometer
+#' Maya2000 Pro  spectrometer s/n MAYP112785
 #'
 #' List of instrument descriptors, each one containing the calibration
-#' data needed to convert raw data acquired on different dates.  Calibration
-#' data from STUK's non-ionising radiation laboratory.
+#' data needed to convert raw data acquired on different dates. Calibration
+#' data from STUK's non-ionising radiation laboratory. Correction method
+#' algorithms for stray light and slit function by Lasse Ylianttila, and
+#' variations by Pedro J. Aphalo..
 #'
 #' @docType data
 #' @keywords datasets
-#' @format A list.
+#' @format Lists and data frames.
 #'
 #' @family objects containing instrument-specific data
 #'
 "MAYP112785_descriptors"
 
-#' Dates for which specific calibrations are valid.
-#'
-#' Data frame with data used to match raw spectral data to different
-#' calibrations based on the date data was acquired.
-#'
-#' @docType data
-#' @keywords datasets
-#' @format A list.
-#'
-#' @family objects containing instrument-specific data
+#' @rdname MAYP112785_descriptors
 #'
 "MAYP112785_calib_dates.df"
 
-#' Correction method definition
-#'
-#' List with data needed to process raw data in the example files. It
-#' contains constants and functions defining the method developed by Lasse
-#' Ylianttila.
-#'
-#' @docType data
-#' @keywords datasets
-#' @format A list.
-#'
-#' @family objects containing instrument-specific data
+#' @rdname MAYP112785_descriptors
 #'
 "MAYP112785_ylianttila.mthd"
 
-#' Correction method definition
-#'
-#' List with data needed to process raw data in the example files. It
-#' contains constants and functions defining a derivative of the method
-#' developed by Lasse Ylianttila.
-#'
-#' @docType data
-#' @keywords datasets
-#' @format A list.
-#'
-#' @family objects containing instrument-specific data
+#' @rdname MAYP112785_descriptors
 #'
 "MAYP112785_sun.mthd"
 
-#' Correction method definition
-#'
-#' List with data needed to process raw data in the example files. It
-#' contains constants and functions defining a simplified method that includes
-#' some corrections developed by Lasse Ylianttila, but is less effective in
-#' controlling stray light. Should be used only when more advanced methods
-#' cannot be applied.
-#'
-#' @docType data
-#' @keywords datasets
-#' @format A list.
-#'
-#' @family objects containing instrument-specific data
+#' @rdname MAYP112785_descriptors
 #'
 "MAYP112785_simple.mthd"
 
 #######################
 
-#' Instrument descriptors for a Maya2000 Pro  spectrometer
+#' Maya2000 Pro spectrometer s/n MAYP114590
 #'
 #' List of instrument descriptors, each one containing the calibration
-#' data needed to convert raw data acquired on different dates. Calibration data
-#' from Ocean Optics (currently Ocean Insight).
+#' data needed to convert raw data acquired on different dates. Calibration
+#' data from STUK's non-ionising radiation laboratory. Correction method
+#' algorithms for stray light and slit function by Lasse Ylianttila, and
+#' variations by Pedro J. Aphalo..
+#'
+#' @note Some of the method definitions are fill-ins for methods not implemented.
+#' In this case no characterization of the slit function is available.
 #'
 #' @docType data
 #' @keywords datasets
-#' @format A list.
+#' @format Lists and data frames.
 #'
 #' @family objects containing instrument-specific data
 #'
 "MAYP114590_descriptors"
 
-#' Instrument calibration for a Maya 2000 Pro spectrometer
-#'
-#' This a calibration spectrum, from a calibration done at Ocean Optics
-#' (currently Ocean Insight).
-#'
-#' @docType data
-#' @keywords datasets
-#' @format A \code{calibration_spct} object.
-#'
-#' @family objects containing instrument-specific data
+#' @rdname MAYP114590_descriptors
 #'
 "MAYP114590_cal.spct"
 
-#' Dates for which specific calibrations are valid.
-#'
-#' Data frame with data used to match raw spectral data to different
-#' calibrations based on the date data was acquired.
-#'
-#' @docType data
-#' @keywords datasets
-#' @format A list.
-#'
-#' @family objects containing instrument-specific data
+#' @rdname MAYP114590_descriptors
 #'
 "MAYP114590_calib_dates.df"
 
-#' Correction method definition
-#'
-#' List with data needed to process raw data in the example files. It
-#' contains constants and functions defining the method developed by Lasse
-#' Ylianttila. This spectrometer was not fully characterized and some
-#' parameters used are those obtained for similarly configures spectrometers
-#' of the same model.
-#'
-#' @docType data
-#' @keywords datasets
-#' @format A list.
-#'
-#' @family objects containing instrument-specific data
+#' @rdname MAYP114590_descriptors
 #'
 "MAYP114590_ylianttila.mthd"
 
-#' Correction method definition
-#'
-#' List with data needed to process raw data in the example files. It
-#' contains constants and functions defining a derivative of the method
-#' developed by Lasse Ylianttila.
-#'
-#' @docType data
-#' @keywords datasets
-#' @format A list.
-#'
-#' @family objects containing instrument-specific data
+#' @rdname MAYP114590_descriptors
 #'
 "MAYP114590_sun.mthd"
 
-#' Correction method definition
-#'
-#' List with data needed to process raw data in the example files. It
-#' contains constants and functions defining a simplified method that includes
-#' some corrections developed by Lasse Ylianttila, but is less effective in
-#' controlling stray light. Should be used only when more advanced methods
-#' cannot be applied.
-#'
-#' @docType data
-#' @keywords datasets
-#' @format A list.
-#'
-#' @family objects containing instrument-specific data
+#' @rdname MAYP114590_descriptors
 #'
 "MAYP114590_simple.mthd"
 
-#' Correction method definition
-#'
-#' List with data needed to process raw data in the example files. It
-#' contains constants and functions defining a method implementing only
-#' minimal corrections for stray light and no correction for the slit function.
-#'
-#' @docType data
-#' @keywords datasets
-#' @format A list.
-#'
-#' @family objects containing instrument-specific data
+#' @rdname MAYP114590_descriptors
 #'
 "MAYP114590_none.mthd"
 
